70c20977c6dc013f9a8c6c9d987c6dc13b5645bc
Jan Algermissen <jan.algermissen@ecociel.ch>
2025-11-22 11:35:26 +0100

    Prepare initial publish

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,24 @@
 pub mod error;
 pub mod executor;
-pub mod manager;
 pub mod jobs;
+pub mod manager;
 pub mod repo;
 pub mod schedule;
 
-pub use repo::cassandra;
+use serde::{Deserialize, Serialize};
+use std::fmt;
 
 pub use jobs::{JobCfg, JobMetadata};
-use serde::{Deserialize, Serialize};
+pub use repo::cassandra;
 
-#[derive(PartialEq, Eq,Debug, Clone,Serialize,Deserialize)]
+#[derive(PartialEq, Eq, Debug, Clone, Serialize, Deserialize)]
 pub struct JobName(pub String);
-
-use std::fmt;
 
 impl fmt::Display for JobName {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
+        write!(f, "{}", self.0)
     }
 }
-
 
 impl JobName {
     pub fn as_str(&self) -> &str {
